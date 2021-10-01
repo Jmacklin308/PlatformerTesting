03a2045d3449021e87167d13a49bfd5600fe55cf
Justin Macklin <Jmacklin308>
2021-09-30 21:02:59 -0500

    Initial commit

---
+++ scripts/Player.gd
@@ -0,0 +1,13 @@
+extends CollisionShape2D
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    pass # Replace with function body.
+
+
+
+#update movement
+func _physics_process(delta: float) -> void:
